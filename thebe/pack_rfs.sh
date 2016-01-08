27774c519eea9f6ebd99a6b518e7d36b4cab90a7
Pat O'Neil <oneil@hughes.com>
2016-01-08 12:27:23 -0500

    Newest RFS with pcre and grown-up traceroute
    pcre-config needed tweaking because pcre is not installed canonically.
    traceroute6 is in. Still looking for traceroute[4], so there may be
    more changes yet, but as Brezhnev said: What we have, we keep.

--- thebe/pack_rfs.sh
+++ thebe/pack_rfs.sh
@@ -4,7 +4,7 @@
 #I pack up the toolchain into a single archive. Pathes are applied
 # and permissions are set correctly before re-packing.
 
-SYSROOTS=/opt/poky/1.6.1/sysroots
+SYSROOTS=/opt/poky/1.7/sysroots
 TARGETROOT=armv7a-vfp-neon-poky-linux-gnueabi
 BAKEDRFS=../build/tmp/deploy/images/qemuarm/core-image-minimal-dev-qemuarm.tar.bz2
 TOOLSPLUS=thebe-toolchain-and-rfs.tar.bz2
@@ -27,11 +27,15 @@ patch -p1 <../../thebe_oe.patch
 cd ../..
 echo ...patches added
 
+#pcre has a funny configuration file that has been adjusted for
+# our peculiar circumstances
+cp pcre-config sysroots/$TARGETROOT/usr/bin
+
 #These are only half-baked. They got built, but not included in the RFS.
-GLIBC=../build/tmp/work/armv5te-poky-linux-gnueabi/glibc/2.20-r0/package
+GLIBC=../build/tmp/work/armv7a-vfp-poky-linux-gnueabi/glibc/2.20-r0/package
 cp $GLIBC/usr/bin/getent sysroots/$TARGETROOT/usr/bin
 
-OPENSSH=../build/./tmp/work/armv5te-poky-linux-gnueabi/openssh/6.6p1-r0/package
+OPENSSH=../build/./tmp/work/armv7a-vfp-poky-linux-gnueabi/openssh/6.6p1-r0/package
 mkdir -p sysroots/$TARGETROOT/usr/lib/openssh
 cp $OPENSSH/usr/lib/openssh/sftp-server sysroots/$TARGETROOT/usr/lib/openssh
 
